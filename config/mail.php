2f0fbc134028153cdfb75e2313429bc259f305c7
alifarda126 <alifarda126@gmail.com>
2026-05-24 12:09:16 +0700

    Fix mail scheme for tls in symfony mailer

--- config/mail.php
+++ config/mail.php
@@ -39,7 +39,7 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
+            'scheme' => env('MAIL_SCHEME', 'smtps') === 'tls' ? 'smtps' : env('MAIL_SCHEME', env('MAIL_ENCRYPTION', 'smtps')),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
