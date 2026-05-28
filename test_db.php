495244887eb32e251d30a024a943d4cc81390c97
alifarda126 <alifarda126@gmail.com>
2026-05-28 18:55:37 +0700

    fix s3

--- test_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$org = \App\Models\Organisasi::first();
-echo 'Path di DB: ' . $org->logo . PHP_EOL;
-echo 'URL yg di-generate: ' . $org->logo_url . PHP_EOL;
