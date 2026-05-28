6439ef0cae96b67fd37cee1efd043bef700114b5
alifarda126 <alifarda126@gmail.com>
2026-05-28 16:33:47 +0700

    migration fix

--- config/database.php
+++ config/database.php
@@ -60,8 +60,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA', true),
-                PDO::ATTR_SSL_VERIFY_SERVER_CERT => false,
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => storage_path('tidb-ca.pem'),
+                PDO::ATTR_SSL_VERIFY_SERVER_CERT => true,
             ]) : [],
         ],
 
@@ -81,8 +81,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA', true),
-                PDO::ATTR_SSL_VERIFY_SERVER_CERT => false,
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => storage_path('tidb-ca.pem'),
+                PDO::ATTR_SSL_VERIFY_SERVER_CERT => true,
             ]) : [],
         ],
 
